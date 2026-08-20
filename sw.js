e4d2441092faefb66af33a9427371a9048921ac7
blessed-ui <blessedbeatify@gmail.com>
2026-08-20 00:54:51 -0400

    Fix Convergence primary practice button v17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='convergence-pwa-v20';
+const CACHE='convergence-pwa-v21';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./convergence-wallpaper.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
